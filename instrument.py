b261de5517d8a94dc963c0d5f28450bb8e1c6d2a
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2025-02-16 20:37:59 -0500

    Run black. Be more tolerant of comments on label lines

--- instrument.py
+++ instrument.py
@@ -2,6 +2,7 @@ import sys
 import re
 import itertools
 
+
 def main() -> None:
     if len(sys.argv) != 2:
         print(f"Usage: python3 {sys.argv[0]} <assembly_file>", file=sys.stderr)
@@ -9,10 +10,14 @@ def main() -> None:
 
     with open(sys.argv[1], "rb") as f:
         source_code: bytes = f.read()
-    
+
     lines: list[bytes] = source_code.splitlines(keepends=True)
 
-    symbols: list[bytes] = [line.split()[1] for line in lines if len(line.split()) == 2 and line.split()[0] in (b".globl", b".global")]
+    symbols: list[bytes] = [
+        tokens[1]
+        for tokens in map(bytes.split, lines)
+        if len(tokens) >= 2 and tokens[0].lower() in (b".globl", b".global") and tokens[1] != b"_start"
+    ]
 
     for line, next_line in itertools.zip_longest(lines, lines[1:]):
         if next_line is None:
@@ -20,10 +25,15 @@ def main() -> None:
         sys.stdout.buffer.write(line)
         tokens: list[bytes] = line.split()
         if len(tokens) >= 1 and tokens[0].endswith(b":") and tokens[0][:-1] in symbols:
-            whitespace_prefix_match: re.Match[bytes] | None = re.match(rb"^(?P<whitespace_prefix>\s*)", next_line)
+            whitespace_prefix_match: re.Match[bytes] | None = re.match(
+                rb"^(?P<whitespace_prefix>\s*)", next_line
+            )
             assert whitespace_prefix_match is not None
             whitespace_prefix: bytes = whitespace_prefix_match["whitespace_prefix"]
-            sys.stdout.buffer.write(whitespace_prefix + b"call abisan_function_entry # Added by preprocess.py\n")
+            sys.stdout.buffer.write(
+                whitespace_prefix + b"call abisan_function_entry # Added by preprocess.py\n"
+            )
+
 
 if __name__ == "__main__":
     main()
