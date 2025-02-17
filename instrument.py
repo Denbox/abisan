e51f12408da2d04ffabbd3868f13643458edb9d8
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2025-02-17 09:07:13 -0500

    Remove the comment inserted by the instrumentation because it breaks nasm compatibility

--- instrument.py
+++ instrument.py
@@ -9,6 +9,13 @@ def get_label(line: bytes) -> bytes | None:
     return None
 
 
+def get_global(line: bytes) -> bytes | None:
+    tokens: list[bytes] = line.split()
+    if len(tokens) >= 2 and tokens[0].lower() in (b".globl", b".global") and tokens[1] != b"_start":
+        return tokens[1]
+    return None
+
+
 def main() -> None:
     if len(sys.argv) != 2:
         print(f"Usage: python3 {sys.argv[0]} <assembly_file>", file=sys.stderr)
@@ -18,28 +25,21 @@ def main() -> None:
         source_code: bytes = f.read()
 
     lines: list[bytes] = source_code.splitlines(keepends=True)
-
-    symbols: list[bytes] = [
-        tokens[1]
-        for tokens in map(bytes.split, lines)
-        if len(tokens) >= 2 and tokens[0].lower() in (b".globl", b".global") and tokens[1] != b"_start"
-    ]
+    global_symbols: list[bytes] = [symbol for symbol in map(get_global, lines) if symbol is not None]
 
     for i, line in enumerate(lines):
-        next_line: bytes = b"    "
+        next_line: bytes = b"    "  # The next nonempty, non-label line (used to infer indentation)
         for j in range(i, len(lines)):
             if not lines[j].isspace() and get_label(lines[j]) is None:
                 next_line = lines[j]
         sys.stdout.buffer.write(line)
-        if get_label(line) in symbols:
+        if get_label(line) in global_symbols:
             whitespace_prefix_match: re.Match[bytes] | None = re.match(
                 rb"^(?P<whitespace_prefix>\s*)", next_line
             )
             assert whitespace_prefix_match is not None
             whitespace_prefix: bytes = whitespace_prefix_match["whitespace_prefix"]
-            sys.stdout.buffer.write(
-                whitespace_prefix + b"call abisan_function_entry # Added by preprocess.py\n"
-            )
+            sys.stdout.buffer.write(whitespace_prefix + b"call abisan_function_entry\n")
 
 
 if __name__ == "__main__":
