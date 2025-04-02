93148cb0cf7e67b8e48f2a88489d3fad76242a7d
shselbst <shselbst@gmail.com>
2025-04-02 16:17:06 -0400

    test now works for any function, assumes all calls are to abi compliant funcs

--- tests/tainted_rdi_into_heap/tainted_rdi_into_heap.s
+++ tests/tainted_rdi_into_heap/tainted_rdi_into_heap.s
@@ -1,16 +1,8 @@
 .intel_syntax noprefix
 
-.globl f
-f:
-    ret
-
 .globl tainted_rdi_into_heap
 tainted_rdi_into_heap:
-    # TODO: remove f from this test. malloc should be enough to taint rdi.
     mov rdi, 8
     call malloc
-    push rax
-    call f
-    pop rax
     mov qword ptr [rax], rdi
     ret
