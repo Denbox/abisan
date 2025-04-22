b2ef979ede81c655bb67e4524e82d38018de14d0
shselbst <93411312+shselbst@users.noreply.github.com>
2025-04-22 15:16:47 -0400

    Delete tests/acccess_below_red_zone directory

--- tests/acccess_below_red_zone/access_below_red_zone.s
+++
@@ -1,6 +0,0 @@
-.intel_syntax noprefix
-
-.globl access_below_red_zone
-access_below_red_zone:
-    mov al, BYTE PTR [rsp - 0x81]
-    ret
--- tests/acccess_below_red_zone/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <assert.h>
-
-void access_below_red_zone(void);
-
-int main(void) {
-    access_below_red_zone();
-}
