17d678effdf6cd01b6fb428f9c51bc4b37358736
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2025-05-15 12:59:43 -0400

    update asm_re

--- asm_re.py
+++ asm_re.py
@@ -20,7 +20,7 @@ SEGMENT_REGISTERS: tuple[str, ...] = (
     'ds',
 )
 
-REGISTERS: tuple[str, ...] = SEGMENT_REGISTERS + ('ah', 'al', 'ax', 'bh', 'bl', 'bp', 'bpl', 'bx', 'ch', 'cl', 'cx', 'dh', 'di', 'dil', 'dl', 'dx', 'eax', 'ebp', 'ebx', 'ecx', 'edi', 'edx', 'eflags', 'eip', 'eiz', 'esi', 'esp', 'fpsw', 'ip', 'rax', 'rbp', 'rbx', 'rcx', 'rdi', 'rdx', 'rip', 'riz', 'rsi', 'rsp', 'si', 'sil', 'sp', 'spl', 'cr0', 'cr1', 'cr2', 'cr3', 'cr4', 'cr5', 'cr6', 'cr7', 'cr8', 'cr9', 'cr10', 'cr11', 'cr12', 'cr13', 'cr14', 'cr15', 'dr0', 'dr1', 'dr2', 'dr3', 'dr4', 'dr5', 'dr6', 'dr7', 'dr8', 'dr9', 'dr10', 'dr11', 'dr12', 'dr13', 'dr14', 'dr15', 'fp0', 'fp1', 'fp2', 'fp3', 'fp4', 'fp5', 'fp6', 'fp7', 'k0', 'k1', 'k2', 'k3', 'k4', 'k5', 'k6', 'k7', 'mm0', 'mm1', 'mm2', 'mm3', 'mm4', 'mm5', 'mm6', 'mm7', 'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15', 'st0', 'st1', 'st2', 'st3', 'st4', 'st5', 'st6', 'st7', 'xmm0', 'xmm1', 'xmm2', 'xmm3', 'xmm4', 'xmm5', 'xmm6', 'xmm7', 'xmm8', 'xmm9', 'xmm10', 'xmm11', 'xmm12', 'xmm13', 'xmm14', 'xmm15', 'xmm16', 'xmm17', 'xmm18', 'xmm19', 'xmm20', 'xmm21', 'xmm22', 'xmm23', 'xmm24', 'xmm25', 'xmm26', 'xmm27', 'xmm28', 'xmm29', 'xmm30', 'xmm31', 'ymm0', 'ymm1', 'ymm2', 'ymm3', 'ymm4', 'ymm5', 'ymm6', 'ymm7', 'ymm8', 'ymm9', 'ymm10', 'ymm11', 'ymm12', 'ymm13', 'ymm14', 'ymm15', 'ymm16', 'ymm17', 'ymm18', 'ymm19', 'ymm20', 'ymm21', 'ymm22', 'ymm23', 'ymm24', 'ymm25', 'ymm26', 'ymm27', 'ymm28', 'ymm29', 'ymm30', 'ymm31', 'zmm0', 'zmm1', 'zmm2', 'zmm3', 'zmm4', 'zmm5', 'zmm6', 'zmm7', 'zmm8', 'zmm9', 'zmm10', 'zmm11', 'zmm12', 'zmm13', 'zmm14', 'zmm15', 'zmm16', 'zmm17', 'zmm18', 'zmm19', 'zmm20', 'zmm21', 'zmm22', 'zmm23', 'zmm24', 'zmm25', 'zmm26', 'zmm27', 'zmm28', 'zmm29', 'zmm30', 'zmm31', 'r8b', 'r9b', 'r10b', 'r11b', 'r12b', 'r13b', 'r14b', 'r15b', 'r8d', 'r9d', 'r10d', 'r11d', 'r12d', 'r13d', 'r14d', 'r15d', 'r8w', 'r9w', 'r10w', 'r11w', 'r12w', 'r13w', 'r14w', 'r15w', 'bnd0', 'bnd1', 'bnd2', 'bnd3')
+REGISTERS: tuple[str, ...] = ('ah', 'al', 'ax', 'bh', 'bl', 'bp', 'bpl', 'bx', 'ch', 'cl', 'cx', 'dh', 'di', 'dil', 'dl', 'dx', 'eax', 'ebp', 'ebx', 'ecx', 'edi', 'edx', 'eflags', 'eip', 'eiz', 'esi', 'esp', 'fpsw', 'ip', 'rax', 'rbp', 'rbx', 'rcx', 'rdi', 'rdx', 'rip', 'riz', 'rsi', 'rsp', 'si', 'sil', 'sp', 'spl', 'cr0', 'cr1', 'cr2', 'cr3', 'cr4', 'cr5', 'cr6', 'cr7', 'cr8', 'cr9', 'cr10', 'cr11', 'cr12', 'cr13', 'cr14', 'cr15', 'dr0', 'dr1', 'dr2', 'dr3', 'dr4', 'dr5', 'dr6', 'dr7', 'dr8', 'dr9', 'dr10', 'dr11', 'dr12', 'dr13', 'dr14', 'dr15', 'fp0', 'fp1', 'fp2', 'fp3', 'fp4', 'fp5', 'fp6', 'fp7', 'k0', 'k1', 'k2', 'k3', 'k4', 'k5', 'k6', 'k7', 'mm0', 'mm1', 'mm2', 'mm3', 'mm4', 'mm5', 'mm6', 'mm7', 'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15', 'st0', 'st1', 'st2', 'st3', 'st4', 'st5', 'st6', 'st7', 'xmm0', 'xmm1', 'xmm2', 'xmm3', 'xmm4', 'xmm5', 'xmm6', 'xmm7', 'xmm8', 'xmm9', 'xmm10', 'xmm11', 'xmm12', 'xmm13', 'xmm14', 'xmm15', 'xmm16', 'xmm17', 'xmm18', 'xmm19', 'xmm20', 'xmm21', 'xmm22', 'xmm23', 'xmm24', 'xmm25', 'xmm26', 'xmm27', 'xmm28', 'xmm29', 'xmm30', 'xmm31', 'ymm0', 'ymm1', 'ymm2', 'ymm3', 'ymm4', 'ymm5', 'ymm6', 'ymm7', 'ymm8', 'ymm9', 'ymm10', 'ymm11', 'ymm12', 'ymm13', 'ymm14', 'ymm15', 'ymm16', 'ymm17', 'ymm18', 'ymm19', 'ymm20', 'ymm21', 'ymm22', 'ymm23', 'ymm24', 'ymm25', 'ymm26', 'ymm27', 'ymm28', 'ymm29', 'ymm30', 'ymm31', 'zmm0', 'zmm1', 'zmm2', 'zmm3', 'zmm4', 'zmm5', 'zmm6', 'zmm7', 'zmm8', 'zmm9', 'zmm10', 'zmm11', 'zmm12', 'zmm13', 'zmm14', 'zmm15', 'zmm16', 'zmm17', 'zmm18', 'zmm19', 'zmm20', 'zmm21', 'zmm22', 'zmm23', 'zmm24', 'zmm25', 'zmm26', 'zmm27', 'zmm28', 'zmm29', 'zmm30', 'zmm31', 'r8b', 'r9b', 'r10b', 'r11b', 'r12b', 'r13b', 'r14b', 'r15b', 'r8d', 'r9d', 'r10d', 'r11d', 'r12d', 'r13d', 'r14d', 'r15d', 'r8w', 'r9w', 'r10w', 'r11w', 'r12w', 'r13w', 'r14w', 'r15w', 'bnd0', 'bnd1', 'bnd2', 'bnd3')
 
 _REGISTER: str = rf"(?:{'|'.join(REGISTERS)})"
 
@@ -45,38 +45,41 @@ _LINE_SUFFIX: str = rf"(?:[ \t]*{_COMMENT}?\n?)\Z"
 
 _OFFSET: str = r"(?:offset)"
 
-_OFFSET_SEQUENCE: str = rf"(?:{_OFFSET}(?:[ \t]+{_OFFSET})*)"
-
-# XXX: OWORD TBYTE MMWORD and so on.
-_WIDTH: str = r"(?:byte|word|dword|qword|xmmword|ymmword|zmmword)"
-
 _PTR: str = r"(?:ptr)"
 
-_MEMORY_OPERAND_MODIFIER: str = rf"(?:(?:{_OFFSET_SEQUENCE}[ \t]+)?(?:{_WIDTH}(?:[ \t]+{_PTR})?)?(?:[ \t]+{_OFFSET_SEQUENCE})?)"
+# TODO: OWORD TBYTE MMWORD and so on.
+_WIDTH: str = rf"(?:(?:byte|word|dword|qword|xmmword|ymmword|zmmword)(?:[ \t]+{_PTR})?)"
+
+_MEMORY_OPERAND_MODIFIER: str = rf"(?:{_WIDTH}|{_OFFSET})"
+
+# XXX: This is intentional; GNU asm legitimately allows multiple WIDTH PTR pairs.
+_MEMORY_OPERAND_MODIFIER_SEQUENCE: str = rf"(?:(?:{_MEMORY_OPERAND_MODIFIER}(?:[ \t]+{_MEMORY_OPERAND_MODIFIER})*)?)"
 
 _CONSTANT: str = rf"(?:{_LABEL}|{_IMMEDIATE})"
 
 _OPERATOR: str = r"(?:[-+*/^&<>%|!]|<<|>>)"
 
-_CONSTANT_EXPRESSION: str = rf"(?:{_CONSTANT}(?:[ \t]*{_OPERATOR}[ \t]*{_CONSTANT})*)"
+_CONSTANT_EXPRESSION: str = rf"(?:(?:{_CONSTANT}(?:[ \t]*{_OPERATOR}[ \t]*{_CONSTANT})*)?)"
 
-# XXX: This doesn't enforce matching brackets, which is impossible with re
-_ONE_OR_MORE_OPEN_BRACKETS: str = r"(?:\[(?:[ \t]*\[)*)"
-_ONE_OR_MORE_CLOSE_BRACKETS: str = r"(?:\](?:[ \t]*\])*)"
+_SEGMENT_COLON: str = rf"(?:(?P<OP_NUM_PLACEHOLDER_segment>{_SEGMENT_REGISTER})[ \t]*:)"
 
-_EFFECTIVE_ADDRESS_WITH_SEGMENT_REGISTER: str = rf"(?:(?:{_ONE_OR_MORE_OPEN_BRACKETS}[ \t]*)?{_SEGMENT_REGISTER}[ \t]*:[ \t]*{_CONSTANT_EXPRESSION}(?:[ \t]*{_ONE_OR_MORE_CLOSE_BRACKETS})?)"
+_EFFECTIVE_ADDRESS_DISPLACEMENT_FORM: str = rf"(?:\[?(?P<OP_NUM_PLACEHOLDER_displacement_form_displacement>{_CONSTANT_EXPRESSION})\]?)"
+
+_EFFECTIVE_ADDRESS_BASE_FORM: str = rf"(?:\[[ \t]*(?P<OP_NUM_PLACEHOLDER_base_form_base>{_REGISTER})[ \t]*\])"
 
 # TODO: All the other forms of memory operands
-_EFFECTIVE_ADDRESS: str = rf"(?:{_EFFECTIVE_ADDRESS_WITH_SEGMENT_REGISTER})"
+_EFFECTIVE_ADDRESS: str = rf"(?:(?:{_SEGMENT_COLON}[ \t]*)?(?:{_EFFECTIVE_ADDRESS_BASE_FORM}|{_EFFECTIVE_ADDRESS_DISPLACEMENT_FORM}))"
 
 # XXX: This will allow `offset qword ptrfs:0x10`
-_MEMORY_OPERAND: str = rf"(?:(?:{_MEMORY_OPERAND_MODIFIER}[ \t]*)?{_EFFECTIVE_ADDRESS})"
+_MEMORY_OPERAND: str = rf"(?:(?:{_MEMORY_OPERAND_MODIFIER_SEQUENCE}[ \t]*)?{_EFFECTIVE_ADDRESS})"
 
-_OPERAND: str = rf"(?:{_IMMEDIATE}|{_REGISTER}|{_LABEL}|{_MEMORY_OPERAND})"
+_OPERAND_1: str = rf"(?:{_IMMEDIATE}|{_REGISTER}|{_LABEL}|{_MEMORY_OPERAND})".replace("OP_NUM_PLACEHOLDER", "operand_1")
+_OPERAND_2: str = rf"(?:{_IMMEDIATE}|{_REGISTER}|{_LABEL}|{_MEMORY_OPERAND})".replace("OP_NUM_PLACEHOLDER", "operand_2")
+_OPERAND_3: str = rf"(?:{_IMMEDIATE}|{_REGISTER}|{_LABEL}|{_MEMORY_OPERAND})".replace("OP_NUM_PLACEHOLDER", "operand_3")
 
 _INSTRUCTION_PREFIX: str = "(?:rep(?:n?[ez])?|lock|notrack|cs|data16|addr32)"
 
 # XXX: This allows any number of instruction prefixes
-_INSTRUCTION_STATEMENT: str = rf"(?:(?:{_INSTRUCTION_PREFIX}[ \t]+)*{_MNEMONIC}(?:[ \t]+(?P<operand_1>{_OPERAND})(?:[ \t]*,[ \t]*(?P<operand_2>{_OPERAND})(?:[ \t]*,[ \t]*(?P<operand_3>{_OPERAND}))?)?)?)"
+_INSTRUCTION_STATEMENT: str = rf"(?:(?:{_INSTRUCTION_PREFIX}[ \t]+)*{_MNEMONIC}(?:[ \t]+(?P<operand_1>{_OPERAND_1})(?:[ \t]*,[ \t]*(?P<operand_2>{_OPERAND_2})(?:[ \t]*,[ \t]*(?P<operand_3>{_OPERAND_3}))?)?)?)"
 
 _LINE: str = rf"(?i)(?:{_LINE_PREFIX}(?:{_LABEL_STATEMENT}|{_DIRECTIVE_STATEMENT}|{_INSTRUCTION_STATEMENT})?{_LINE_SUFFIX})"
